3a98f731a12a21664d06036e027cb738d15e5085
Jonas Witt <jonas@metaquark.de>
2014-09-13 19:05:35 +0200

    add cube discovery logic to API call

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 
 var express = require('express');
 var net = require('net');
+var dgram = require('dgram');
 var split = require('split');
 var through = require('through');
 var es = require('event-stream');
@@ -10,6 +11,46 @@ var maxparser = require('./max/parser');
 var PORT = 62910;
 
 var app = express();
+
+app.get('/cubes', function(req, res) {
+  var cubes = [];
+  var client = dgram.createSocket('udp4');
+
+  client.on('message', function(msg, rinfo) {
+    var signature = msg.slice(0, 8).toString('ascii')
+    if (signature != 'eQ3MaxAp') return;
+
+    var firmware = msg.slice(24, 26).toString('hex')
+    var port = firmware > '010e' ? 62910 : 80;
+
+    cubes.push({'address': rinfo.address, 'port': port, 'firmware': firmware})
+  });
+
+  client.on("error", function (err) {
+    console.log(err.stack);
+  });
+
+  client.bind(23272, function () {
+    client.setBroadcast(true)
+    client.setTTL(5)
+
+    client.addMembership('224.0.0.1')
+
+    var message = new Buffer("6551334d61782a002a2a2a2a2a2a2a2a2a2a49", "hex");
+
+    client.send(message, 0, message.length, 23272, '224.0.0.1', function(err, bytes) {
+      if (err) console.log(err);
+    });
+    client.send(message, 0, message.length, 23272, '255.255.255.255', function(err, bytes) {
+      if (err) console.log(err);
+    });
+  })
+
+  setTimeout(function() {
+    client.close();
+    res.json(cubes)
+  }, 2000);
+})
 
 app.get('/cubes/:ip', function(req, res) {
   var parser = new maxparser();
